158dcb778d57fcb3523cb5387926b0a251a9528f
Jordana1TDS <jordana.maia@escola.pr.gov.br>
2026-03-26 11:18:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("btnBuscar").addEventListener("click", function(){
+
+    fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
+    .then(res => res.json())
+    .then(posts => {
+
+        // convertendo para padrão JSON:API
+        const jsonApiFormat = {
+            data: posts.map(post => ({
+            type: "post",
+            id: post.id,
+            attributes: {
+                title: post.title,
+                body: post.body,
+            },
+            relationships: {
+                author: {
+                    data:  {
+                        type: "users",
+                        id: post.userId,  
+                    }
+                }
+            }
+            }))
+        };
+
+        document.getElementById("saida").textContent =
+        JSON.stringify(jsonApiFormat, null, 4);
+    })
+    .catch(erro => {
+        console.log('Erro', erro);
+    });
+});
